1d5bf95c16b17e6bc03b575910970929d4734e8c
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-11 21:45:43 +0330

    Update video_to_audio_bot.py

--- video_to_audio_bot.py
+++ video_to_audio_bot.py
@@ -1,39 +1,25 @@
 import os
 import tempfile
-from telegram import Update, InputFile
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import InputFile
 
+def handle_video(update, context):
+    video = update.message.video
+    video_file = context.bot.getFile(video.file_id)
 
-TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+    # ذخیره ویدیوی دریافتی در یک فایل موقت
+    with tempfile.NamedTemporaryFile(prefix='video_', suffix='.mp4', delete=False) as video_temp:
+        video_file.download(video_temp.name)
+        video_path = video_temp.name
 
+    # تبدیل ویدیو به فایل صوتی و ذخیره آن در یک فایل موقت
+    with tempfile.NamedTemporaryFile(prefix='audio_', suffix='.ogg', delete=False) as audio_temp:
+        audio_path = audio_temp.name
+        convert_video_to_audio(video_path, audio_path)
 
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text('لطفا ویدیویی که می‌خواهید به صوت تبدیل کنید را ارسال کنید.')
+    # ارسال فایل صوتی به کاربر
+    with open(audio_path, 'rb') as audio_file:
+        context.bot.send_audio(chat_id=update.effective_chat.id, audio=InputFile(audio_file))
 
-
-def convert_video_to_audio(update: Update, context: CallbackContext):
-    video_file = update.message.video.get_file()
-    with tempfile.TemporaryDirectory() as tmpdir:
-        video_path = os.path.join(tmpdir, 'input_video.mp4')
-        audio_path = os.path.join(tmpdir, 'output_audio.ogg')
-        video_file.download(video_path)
-        
-        os.system(f'ffmpeg -i {video_path} -vn -c:a libopus {audio_path}')
-        
-        with open(audio_path, 'rb') as audio_file:
-            context.bot.send_audio(chat_id=update.message.chat_id, audio=InputFile(audio_file))
-
-
-def main():
-    updater = Updater(TOKEN)
-
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(MessageHandler(Filters.video, convert_video_to_audio))
-
-    updater.start_polling()
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+    # پاک کردن فایل‌های موقت
+    os.remove(video_path)
+    os.remove(audio_path)
