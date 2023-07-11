76dc61cf3ec15fe0dcfee5d048478a8857b2d268
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-12 00:41:51 +0330

    Update video_to_audio_bot.py

--- video_to_audio_bot.py
+++ video_to_audio_bot.py
@@ -8,18 +8,9 @@ TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
 API_ID = 1234567  # جایگزین کنید با API ID دریافتی
 API_HASH = "your_api_hash"  # جایگزین کنید با API Hash دریافتی
 
-AUDIO_FILES_DIR = '/var/www/audio-files'
-SERVER_DOMAIN_OR_IP = 'your_server_domain_or_IP'
-
 def convert_video_to_audio(video_path, audio_path, audio_quality=3):
     command = f'ffmpeg -i {video_path} -vn -c:a libopus -b:a {audio_quality}k {audio_path}'
     subprocess.call(command, shell=True)
-
-def save_audio_file(file_path):
-    file_name = os.path.basename(file_path)
-    destination_path = os.path.join(AUDIO_FILES_DIR, file_name)
-    os.rename(file_path, destination_path)
-    return f'http://{SERVER_DOMAIN_OR_IP}/{file_name}'
 
 client = TelegramClient('bot', API_ID, API_HASH).start(bot_token=TOKEN)
 
@@ -38,9 +29,8 @@ async def handle_video(event):
             audio_path = audio_temp.name
             convert_video_to_audio(video_path, audio_path)
 
-        # ذخیره فایل صوتی در سرور و ارسال لینک دانلود به کاربر
-        download_url = save_audio_file(audio_path)
-        await event.respond(f'فایل صوتی آماده است. لینک دانلود:\n{download_url}')
+        # ارسال فایل صوتی به کاربر
+        await client.send_file(event.chat_id, audio_path, caption="فایل صوتی آماده است.")
 
         # پاک کردن فایل‌های موقت
         os.remove(video_path)
